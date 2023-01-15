4ff27d009f3eac965ac4d25d870b66aa4f4d16cb
TaeYeoN <ihavetwobigdick@gmail.com>
2023-01-15 22:55:34 +0800

    for ASM update

--- ASM/ASM02/q1a.py
+++ ASM/ASM02/q1a.py
@@ -1,5 +1,11 @@
+#Purpose : prints the square with apattern according to the examples below.
+
+#Written by Kenny Ng Wai Yu
+#On 15/1/2023
+#For Assignment 2
 class Square():
 
+#Input
     def __init__(self):
 
         self.__size = input("Enter size of pattern: ")
