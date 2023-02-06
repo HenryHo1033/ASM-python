01e643aa2692599bd35ba284014524fee9eaa95f
HenryHo <heqh@mama.cn>
2023-02-06 09:59:49 +0800

    ASM03 Q1A

---
+++ ASM/ASM03/q1a.py
@@ -0,0 +1,21 @@
+import random
+
+def validate(N):
+
+    assert N > 0
+
+def genRanNumList(N):
+
+    validate(N)
+    random_list = []
+
+    for i in range(N):
+
+        random_list.append(random.randint(1,100))
+
+    return random_list
+
+
+print (genRanNumList(10))
+print (genRanNumList(20))
+print (genRanNumList(30))
