20e182fe9dc8e3d724e8c16ffe94a90f2f1d0f82
HenryHo <heqh@mama.cn>
2023-04-10 11:59:12 +0800

    Complete ASM04 Q1B

---
+++ ASM/ASM04/q1b.py
@@ -0,0 +1,29 @@
+def checkBalancedSymbols(exp):
+
+    stack = []
+    opening = set('([{')
+    closing = set(')]}')
+    matching = {'(': ')', '[': ']', '{': '}'}
+    ignore = False  # Flag to ignore # symbol and anything after it
+
+    for char in exp:
+        if char == '#':
+            ignore = True  # Set ignore flag to True when # symbol is encountered
+        if ignore:
+            continue  # Ignore everything after # symbol
+        if char in opening:
+            stack.append(char)
+        elif char in closing:
+            if not stack:
+                return False
+            last_open = stack.pop()
+            if char != matching[last_open]:
+                return False
+
+    return not stack
+
+
+print (checkBalancedSymbols('[1+(2-3)]'))
+print (checkBalancedSymbols('{2 + [1+(2-3)]'))
+print (checkBalancedSymbols('{2 + [1+(2-3)]} # ignored[[['))
+print (checkBalancedSymbols('{2 + [1+(2-3)]} [[['))
