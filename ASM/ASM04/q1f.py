31c207f0fd8c96b4cd3563dfb70003ecf5b3cc81
HenryHo <heqh@mama.cn>
2023-04-10 13:00:22 +0800

    Complete ASM04 Q1F

---
+++ ASM/ASM04/q1f.py
@@ -0,0 +1,44 @@
+from binarytree import Node, bst
+
+# Part (i)
+root = Node(4)
+root.left = Node(2)
+root.right = Node(6)
+root.left.left = Node(1)
+root.left.right = Node(3)
+root.right.left = Node(5)
+root.right.right = Node(7)
+print(root)
+
+# Part (ii)
+perfect_bst = bst(height=3, is_perfect=True)
+print(perfect_bst)
+
+# Part (iii)
+# 1. Print the total number of nodes
+num_nodes = len(perfect_bst)
+print("Total number of nodes:", num_nodes)
+
+# 2. Print the total number of leaves
+num_leaves = len(perfect_bst.leaves)
+print("Total number of leaves:", num_leaves)
+
+
+# 3. Print the left most node in the tree
+def left_most_node(node):
+    while node.left is not None:
+        node = node.left
+    return node
+
+
+print("Left most node in the tree:", left_most_node(perfect_bst))
+
+
+# 4. Print the right most node in the tree
+def right_most_node(node):
+    while node.right is not None:
+        node = node.right
+    return node
+
+
+print("Right most node in the tree:", right_most_node(perfect_bst))
