6a200fad1f28830a1cfd57e4299da72b60545e68
TaeYeoN <ihavetwobigdick@gmail.com>
2023-04-16 22:22:54 +0800

    test

--- ASM/Testcode/testcode.py
+++ ASM/Testcode/testcode.py
@@ -1,8 +1,8 @@
-s = [1, 1, 2, 1]
-x = 1
-print(s.index(x))
-i = 2
-print(s.index(x,1))
-j = 4
-print(s.index(x, i, j))
-print(s.count(x))
+x = 2
+y = 3
+print (x | y)
+print (x ^ y)
+print (x & y)
+print (x >> y )
+print (x << y)
+print (~x)
